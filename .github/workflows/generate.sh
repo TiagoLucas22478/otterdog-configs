936897079ec04b4d1007f3d88ecd134d8f0a0c73
Thomas Neidhart <thomas.neidhart@gmail.com>
2023-07-24 14:01:04 +0200

    Fix copy&paste error.

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -14,5 +14,5 @@ EOT
 for org in $(jq -c '.organizations[]' < otterdog.json); do
   GITHUB_ID=$(echo $org | jq -r '.github_id')
   ECLIPSE_PROJECT=$(echo $org | jq -r '.eclipse_project')
-  echo "[${ECLIPSE_PROJECT}](https://projects.eclipse.org/projects/${ECLIPSE_PROJECT}),[${GITHUB_ID}](https://github.com/${GITHUB_ID}),[${GITHUB_ID}.jsonnet](https://github.com/${GITHUB_ID}/.eclipsefdn
+  echo "[${ECLIPSE_PROJECT}](https://projects.eclipse.org/projects/${ECLIPSE_PROJECT}),[${GITHUB_ID}](https://github.com/${GITHUB_ID}),[${GITHUB_ID}.jsonnet](https://github.com/${GITHUB_ID}/.eclipsefdn/blob/main/otterdog/${GITHUB_ID}.jsonnet)" >> ${OUTPUT}
 done
