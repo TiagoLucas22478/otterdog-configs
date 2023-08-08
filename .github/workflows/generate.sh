99f6945118f07115a6e5db3921664474a5e3970f
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2023-08-08 09:22:02 +0200

    Add link to GH page.

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -8,11 +8,11 @@ EOT
 
 OUTPUT='./docs/orgs.csv'
 cat <<EOT > ${OUTPUT}
-Eclipse project,GitHub organization,Otterdog configuration
+Eclipse project,GitHub organization,Otterdog configuration,GH Page
 EOT
 
 for org in $(jq -c '.organizations[]' < otterdog.json); do
   GITHUB_ID=$(echo $org | jq -r '.github_id')
   ECLIPSE_PROJECT=$(echo $org | jq -r '.eclipse_project')
-  echo "[${ECLIPSE_PROJECT}](https://projects.eclipse.org/projects/${ECLIPSE_PROJECT}),[${GITHUB_ID}](https://github.com/${GITHUB_ID}),[${GITHUB_ID}.jsonnet](https://github.com/${GITHUB_ID}/.eclipsefdn/blob/main/otterdog/${GITHUB_ID}.jsonnet)" >> ${OUTPUT}
+  echo "[${ECLIPSE_PROJECT}](https://projects.eclipse.org/projects/${ECLIPSE_PROJECT}),[${GITHUB_ID}](https://github.com/${GITHUB_ID}),[${GITHUB_ID}.jsonnet](https://github.com/${GITHUB_ID}/.eclipsefdn/blob/main/otterdog/${GITHUB_ID}.jsonnet),[https://${GITHUB_ID}.github.io/.eclipsefdn](https://${GITHUB_ID}.github.io/.eclipsefdn)" >> ${OUTPUT}
 done
