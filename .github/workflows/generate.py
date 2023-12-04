8973f566ab1dd5164373560683e56bc40c93d331
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2023-12-04 16:41:33 +0100

    Add total counts.

--- .github/workflows/generate.py
+++ .github/workflows/generate.py
@@ -30,18 +30,30 @@ def get_repo_status(token: str, owner: str, repo: str) -> (int, int):
         }
     }"""
 
+    query_total = """query($org: String!, $repo: String!) {
+        repository(owner: $org, name: $repo) {
+            issues(first: 0) {
+                totalCount
+            }
+            pullRequests(first: 0) {
+                totalCount
+            }
+        }
+    }"""
+
     variables = {
         "org": owner,
         "repo": repo
     }
 
     response = requests.post("https://api.github.com/graphql", headers=headers, json={"query": query, "variables": variables})
-
+    response_total = requests.post("https://api.github.com/graphql", headers=headers, json={"query": query_total, "variables": variables})
+    
     data = response.json()["data"]["repository"]
-    if data is not None:
-        return data["issues"]["totalCount"], data["pullRequests"]["totalCount"]
-    else:
-        return 0, 0
+    data_total = response_total.json()["data"]["repository"]
+
+    return (data.get("issues", {}).get("totalCount", 0), data.get("pullRequests", {}).get("totalCount", 0),
+            data_total.get("issues", {}).get("totalCount", 0), data_total.get("pullRequests", {}).get("totalCount", 0))
 
 
 def generate(token: str):
@@ -59,22 +71,32 @@ def generate(token: str):
 
     OUTPUT = './docs/orgs.csv'
     with open(OUTPUT, "w") as out:
-        out.write("| Eclipse project | GitHub organization | Otterdog configuration | Dashboard | Open Issues | Open PRs |\n")
-        out.write("| :-------------- | :------------------ | :--------------------- | :-------: | :---------: | :------: |\n")
+        out.write("| Eclipse project | GitHub organization | Otterdog configuration | Dashboard | Open Issues | Open PRs | Total PRs |\n")
+        out.write("| :-------------- | :------------------ | :--------------------- | :-------: | :---------: | :------: | :-------: |\n")
+
+        openIssueSum = 0
+        openPrSum = 0
+        totalPrSum = 0
 
         for org in organizations:
             github_id = org["github_id"]
             eclipse_project = org["eclipse_project"]
 
-            issueCount, prCount = get_repo_status(token, github_id, ".eclipsefdn")
+            issueCount, prCount, totalIssueCount, totalPRCount = get_repo_status(token, github_id, ".eclipsefdn")
+
+            openIssueSum += issueCount
+            openPrSum += prCount
+            totalPrSum += totalPRCount
 
             out.write(f"| [{eclipse_project}](https://projects.eclipse.org/projects/{eclipse_project}) "
                       f"| [{github_id}](https://github.com/{github_id}) "
                       f"| [{github_id}.jsonnet](https://github.com/{github_id}/.eclipsefdn/blob/main/otterdog/{github_id}.jsonnet) "
                       f"| [:link:](https://{github_id}.github.io/.eclipsefdn) "
                       f"| [{issueCount}](https://github.com/{github_id}/.eclipsefdn/issues) "
-                      f"| [{prCount}](https://github.com/{github_id}/.eclipsefdn/pulls) |\n")
-
+                      f"| [{prCount}](https://github.com/{github_id}/.eclipsefdn/pulls) "
+                      f"| [{totalPRCount}](https://github.com/{github_id}/.eclipsefdn/pulls) |\n")
+                      
+        out.write(f"| Total | | | | {openIssueSum} | {openPrSum} | {totalPrSum} |\n")
 
 if __name__ == "__main__":
     args = sys.argv[1:]
